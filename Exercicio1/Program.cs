789348964f986d6bb6d911b151cca76e58f86469
00112007 <00112007@CIDLAB103M3>
2023-03-23 21:34:23 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 1) if(numero <=9)            {
+                Console.WriteLine("O Numero está dentro da faixa permitida ");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
